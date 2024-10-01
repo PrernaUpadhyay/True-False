8f54e51b451d7b3c16c2b5f5fe4b266ff8e7f0d2
Prerna Upadhyay <156503096+PrernaUpadhyay@users.noreply.github.com>
2024-10-01 20:17:20 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {"text": "Bananas grow on trees.", "answer": "False"},
+    {"text": "Humans share 50% of their DNA with bananas.", "answer": "True"},
+    {"text": "Honey never spoils and can last for thousands of years.", "answer": "True"},
+    {"text": "Octopuses have three hearts.", "answer": "True"},
+    {"text": "The Great Wall of China is visible from the Moon.", "answer": "False"},
+    {"text": "Sharks are mammals.", "answer": "False"},
+    {"text": "Wombat poop is cube-shaped.", "answer": "True"},
+    {"text": "The unicorn is the national animal of Scotland.", "answer": "True"},
+    {"text": "It is possible to sneeze with your eyes open.", "answer": "False"},
+    {"text": "A group of flamingos is called a 'flamboyance'.", "answer": "True"},
+    {"text": "The shortest war in history was between Britain and Zanzibar, lasting only 38 minutes.", "answer": "True"},
+    {"text": "Cows have best friends and can become stressed if separated.", "answer": "True"}
+]
